ac09f6d2e30d161770488593365a1d64282055bf
NuclearCrafter <nuclearcrafter@gmail.com>
2015-07-06 16:38:31 +0200

    Клонирование

---
+++ Analysis/Analysis/Analys.cpp
@@ -0,0 +1,134 @@
+// Analys.cpp: определ€ет точку входа дл€ консольного приложени€.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char filename [128];
+	data arr[2048];
+	data res[1024];
+	double a, b, time, amplitude;
+	int k(0),c(0),t(0);
+    cout << "ENTER INPUT FILENAME" << endl;
+	cin >> filename;
+	fstream file (filename);
+	if (!file)
+	{
+		cout << "INVALID FILENAME. PLEASE RESTART." << endl;
+		system("PAUSE");
+		return 0;
+	}
+	while (!file.eof()) //цикл, выт€гивающий данные из файла в массив структур типа data под названием arr.
+	{                   //в каждом элементе массива хранитс€ врем€ измерени€ и значение амлитуды, соответствующее времени
+		if (file.get() == ':')
+		{
+			file.get();
+			char sign = file.get();
+			if (sign != '-')
+			{
+				file.seekg(int(file.tellg()) - 1);
+			}
+			file >> a;
+			file.seekg(int(file.tellg()) + 1);
+			file >> b;
+			if (k++ % 2 == 0)
+			{
+				time = a + b / 1000;
+				arr[t].time = time;
+			}
+			else
+			{
+				if (sign != '-')
+				{
+					amplitude = a + b / 10;
+				}
+				else
+				{
+					amplitude = -a - b / 10;
+				}
+				arr[t++].amp = amplitude;
+			}
+		}
+	}
+	for (int i = 0; i < t-2;i++) //цикл, выт€гивающий точки максимума из массива значений в массив res
+	{
+		if (arr[i].amp < arr[i+2].amp && arr[i+1].amp <= arr[i+2].amp //условие экстремума (провер€ем окрестность в два измерени€)
+			&&arr[i+3].amp <= arr[i+2].amp && arr[i+4].amp < arr[i+2].amp)
+		{
+			res[c++] = arr[i+2];
+			if (arr[i + 3].amp == arr[i + 2].amp)
+			{
+				i += 2; //перекидываем индекс вперед на два значени€, в случае, если экстремум дублируетс€ в соседней от него точке
+			}
+		}
+	}
+	file.close();
+
+	cout << "DATA ACQUIRED" << endl;
+	cout << "ENTER OUTPUT FILENAME" << endl;
+	cin >> filename;
+	ofstream out(filename);
+	if (!out)
+	{
+		cout << "INVALID FILENAME. PLEASE RESTART." << endl;
+		system("PAUSE");
+		return 0;
+	}
+
+	for (int i = 0; i < c; i++) //выводим данные в файл и на экран 
+	{
+		cout << res[i].time << " " << res[i].amp<<endl;
+		out << res[i].time << " " << res[i].amp << endl;
+	}
+
+	//критерий апериодичности (почти на глаз, проверкой превышени€ тангенса угла наклона некой контрольной величины
+	//(угла наклона пр€мой между 1 и 2 точками))
+	double decRaw = log(res[0].amp / res[1].amp);
+	double endPoint;
+	for (int i = 0; i < c - 4; i++)
+	{
+		if (abs(decRaw / (res[1].time - res[0].time) - log(res[i].amp / res[i + 4].amp) / (res[i + 4].time - res[i].time))>0.1)
+		{
+			endPoint = i;
+		}
+	}
+	//логарфмируем амлитуды и переместим начало координат дл€ ћЌ 
+	double shiftTime = res[0].time;
+	double shiftAmp = log(res[0].amp);
+	for (int i = 0; i < endPoint; i++)
+	{
+		res[i].time -= shiftTime;
+		res[i].amp = log(res[i].amp)-shiftAmp;
+	}
+	//собственно, ћЌ 
+	double MHKresult;
+	double tempChisl(0);
+	double tempZnam(0);
+	for (int i = 0; i < endPoint; i++)
+	{
+		tempChisl += res[i].amp*res[i].time;
+		tempZnam += res[i].time*res[i].time;
+	}
+	MHKresult = tempChisl / tempZnam;
+
+	//ѕродолжаем вывод
+	cout << "DEADPOINT NUMBER (WARNING, FLUCTUATION MAY OCCUR) :" << endPoint << endl; //точка, в которой начинаетс€ апериодичность
+
+	cout << "FOUND " << c << " MAXIMUMS" << endl;
+	out << "FOUND " << c << " MAXIMUMS" << endl;
+
+	cout << "LOGARYTHMIC DECREMENT CALCULATED WITH FIRST TWO AMPLITUDES: " << decRaw << endl;
+	out << "LOGARYTHMIC DECREMENT CALCULATED WITH FIRST TWO AMPLITUDES: " << decRaw << endl;
+
+	cout << "NON-LOGARYTHMIC DECREMENT CALCULATED WITH MHK " << MHKresult << endl;
+	out << "NON-LOGARYTHMIC DECREMENT CALCULATED WITH MHK " << MHKresult << endl;
+
+	cout << "NON-LOGARYTHMIC DECREMENT CALCULATED WITH FIRST TWO AMPLITUDES: " << -decRaw / (res[1].time - res[0].time) << endl;
+	out << "NON-LOGARYTHMIC DECREMENT CALCULATED WITH FIRST TWO AMPLITUDES: " << -decRaw / (res[1].time - res[0].time) << endl;
+	out.close();
+	system("PAUSE");
+	return 0;
+}
+
---
+++ Analysis/Analysis/struct.h
@@ -0,0 +1,5 @@
+struct data
+{
+	double time;
+	double amp;
+};
